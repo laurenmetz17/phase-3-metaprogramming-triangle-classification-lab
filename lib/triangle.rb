64d5a9e5093e8eb3c1613afa1f8a5a1355f35ab2
Lauren Metz <Laurenmetz15@gmail.com>
2023-01-22 19:30:12 -0500

    commit tree 60ca0b1262f961b9edf1e5109259355e33ded387

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
+require 'pry'
 class Triangle
-  # write code here
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if @side1 > 0 && @side2 > 0 && @side3 > 0
+      if (@side1 + @side2) > @side3 && (@side1 + @side3) > @side2 && (@side2 + @side3) > @side1
+        if @side1 == @side2 && @side1 == @side3
+          return :equilateral
+        elsif @side1 == @side2 && @side1 != @side3
+          return :isosceles
+        elsif @side2 == @side3 && @side1 != @side3
+          return :isosceles
+        elsif @side1 == @side3 && @side2 != @side3
+          return :isosceles
+        else
+          return :scalene
+        end
+      else
+        raise TriangleError
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+    "triangle must have postive sides"
+  end
 end
